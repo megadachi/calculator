ecb86c9df4d26f084c12ff1a7631d9a0e6222113
madachi <megadachi@hotmail.com>
2019-08-29 11:23:50 +0900

    Initial Commit

---
+++ calculator/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  calculator
+//
+//  Created by M A on 2019/08/29.
+//  Copyright © 2019 M A. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ calculator/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  calculator
+//
+//  Created by M A on 2019/08/29.
+//  Copyright © 2019 M A. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
